e20f3a8581f39cd1d0bd187867dc755ee53377b4
Iryna <langnareg@gmail.com>
2018-12-16 23:12:04 +0200

    add localStorage

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import Form from "./containers/Form";
 import Search from "./containers/Search";
 import { Provider } from "react-redux";
 import storeCreator from "./store";
+import {loadState, saveState} from './localStorage'
 
-const store = storeCreator();
+const presistedSatate = loadState();
+const store = storeCreator(presistedSatate);
+
+store.subscribe(() => {
+  saveState(store.getState());
+})
 
 class App extends Component {
   render() {
